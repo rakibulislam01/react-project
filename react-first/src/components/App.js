b5a46edebfd8791d9fb7c4cc08170af078b1c37f
rakibulislam01 <mrakibul70@gmail.com>
2020-02-09 00:14:11 +0600

    First React Component - 5

---
+++ react-first/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import First from './First/first'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <First/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-first/src/components/First/first.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+class first extends Component{
+    render(){
+        return (
+            <div>
+                <h3>My name is Rakibul Islam</h3>
+                <p>This is my first react app.</p>
+            </div>
+        )
+    }
+}
+
+export default first
