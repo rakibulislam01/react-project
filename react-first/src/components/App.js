8f1f4982b9ac03f5c6fa70edbeeefa25ec85bf26
rakibulislam01 <mrakibul70@gmail.com>
2020-02-10 01:19:12 +0600

    State in React - part 1(link 7:https://youtu.be/-CLsHGszdds?list=PL_XxuZqN0xVAvcGzTEAyPSOqgUQA08rNB)

--- react-first/src/components/App.js
+++ react-first/src/components/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import First from './First/first'
+import Counter from "./Counter/counter";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <First name='Rakibul Islam' email='test@gmail.com' address='Dhaka'/>
-        <First name='Test' email='test2@gmail.com' address='Uttara,Dhaka'/>
-        <First name='Test2' email='test3@gmail.com' address='Rampura,Dhaka'/>
+        {/*<First name='Rakibul Islam' email='test@gmail.com' address='Dhaka'/>*/}
+        {/*<First name='Test' email='test2@gmail.com' address='Uttara,Dhaka'/>*/}
+        {/*<First name='Test2' email='test3@gmail.com' address='Rampura,Dhaka'/>*/}
+
+        <Counter/>
       </div>
     );
   }
