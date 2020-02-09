745409655d04d0c89a8c1eaf64c3a70130b98619
rakibulislam01 <mrakibul70@gmail.com>
2020-02-10 01:37:41 +0600

    State in React part 2

--- react-first/src/components/App.js
+++ react-first/src/components/App.js
@@ -4,14 +4,25 @@ import First from './First/first'
 import Counter from "./Counter/counter";
 
 class App extends Component {
+  state = {
+    person: [
+      {name:'Rakibul Islam', email:'test@gmail.com', address:'Dhaka'},
+      {name:'Test', email:'test2@gmail.com', address:'Uttara,Dhaka'},
+      {name:'Test2', email:'test3@gmail.com', address:'Rampura,Dhaka'}
+    ]
+  };
+
   render() {
     return (
       <div className="App">
-        {/*<First name='Rakibul Islam' email='test@gmail.com' address='Dhaka'/>*/}
-        {/*<First name='Test' email='test2@gmail.com' address='Uttara,Dhaka'/>*/}
-        {/*<First name='Test2' email='test3@gmail.com' address='Rampura,Dhaka'/>*/}
-
-        <Counter/>
+          { this.state.person.map((people, index) => {
+            return <First
+                key={index}
+                name={people.name}
+                email={people.email}
+                addres={people.address}/>
+          })}
+        {/*<Counter/>*/}
       </div>
     );
   }
