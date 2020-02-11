0bcfc1d4c285eb49d97d9ec7e9b7c1ed31b5e407
rakibulislam01 <mrakibul70@gmail.com>
2020-02-11 23:29:35 +0600

    Styles in React

--- react-first/src/components/App.js
+++ react-first/src/components/App.js
@@ -1,68 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import Example from "./Example_state_less/example";
 
-// import First from './First/first'
-// import Counter from "./Counter/counter";
-
-// class App extends Component {
-//   state = {
-//     person: [
-//       {name:'Rakibul Islam', email:'test@gmail.com', address:'Dhaka'},
-//       {name:'Test', email:'test2@gmail.com', address:'Uttara,Dhaka'},
-//       {name:'Test2', email:'test3@gmail.com', address:'Rampura,Dhaka'}
-//     ]
-//   };
-//
-//   render() {
-//     return (
-//       <div className="App">
-//           { this.state.person.map((people, index) => {
-//             return <First
-//                 key={index}
-//                 name={people.name}
-//                 email={people.email}
-//                 addres={people.address}/>
-//           })}
-//         {/*<Counter/>*/}
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
-
-
-// Target a spacific event
-// Create Handler
-// In every handler we will get an event object
 class App extends Component {
-    state = {
-        name: ''
-    };
-
-    clickHandler = () => {
-        alert('I am clicked')
-    };
-
-    inputHandler = (event) => {
-        this.setState(
-            {
-                name: event.target.value
-            }
-        )
-    };
 
     render() {
+        let obj = {
+            padding: '30px',
+            fontFamily: 'Arial',
+            fontSize: '30px'
+        };
+
         return (
             <div className="App">
-                <input onChange={this.inputHandler} value={this.state.name} type='text' placeholder='Enter your name'/>
-                <button onClick={this.clickHandler}>
-                    click me
-                </button>
-                {this.state.name ? <p>Hello Mr. {this.state.name}</p> : ''}
-
-                <Example name='Md. Rakibul Islam'/>
+                <h1 style={obj}>Hello Programmer</h1>
             </div>
         );
     }
